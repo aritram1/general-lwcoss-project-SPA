a242d7d3ac89b129062502d86755e83022066b34
Aritra Mukherjee <aritramukherjee@Aritras-MacBook-Air.local>
2023-03-26 23:24:33 +0530

    chess board added

--- src/modules/ui/footer/footer.js
+++ src/modules/ui/footer/footer.js
@@ -1,3 +1,22 @@
 import { LightningElement } from 'lwc';
 
-export default class Footer extends LightningElement {}
+export default class Footer extends LightningElement {
+    footerItems = [];
+    connectedCallback(){
+        this.footerItems.push({
+            'label' : 'About Me',
+            'id': 'about_me',
+            'path' : '' 
+        });
+        this.footerItems.push({
+            'label' : 'Credit',
+            'id': 'credit',
+            'path' : '' 
+        });
+        this.footerItems.push({
+            'label' : 'Source',
+            'id': 'source',
+            'path' : '' 
+        });
+    }
+}
